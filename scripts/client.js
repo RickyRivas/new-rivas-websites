584cb7c0a3bd98a97d2695b73ed6af1eefec52eb
Ricky Rivas <rickyrivas918@gmail.com>
2021-06-18 02:38:22 -0500

    Update client.js

--- scripts/client.js
+++ scripts/client.js
@@ -4,3 +4,13 @@ var stripe = Stripe(
     apiVersion: "2020-08-27",
   }
 );
+var paymentRequest = stripe.paymentRequest({
+  country: "US",
+  currency: "usd",
+  total: {
+    label: "Demo total",
+    amount: 1099,
+  },
+  requestPayerName: true,
+  requestPayerEmail: true,
+});
